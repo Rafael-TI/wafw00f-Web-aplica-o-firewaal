6bd97479c6aabceaa94607cdae66377e772c297e
Rafaelti2019 <47398819+Rafael-TI@users.noreply.github.com>
2019-09-12 01:25:10 -0300

    Create Setup.sh
    Leia o arquivo > lei-me.txt

---
+++ Setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo " By srlentest "
+sleep 2
+echo  
+echo  "          /      \ "
+echo   "         (  Woof! ) "
+echo   "          \______/    "                  
+echo "             ,,          "                
+ echo "          .-. -    _______      "           
+  echo "        ()``; |==|_______)      "          
+  echo "        / ('        /|\        "          
+  echo "     (  /  )        / | \      "            
+  echo "       \(_)_))      /  |  \ |__| "
+  sleep 4 
+  echo " atualizando seus pacotes "
+  echo
+  apt update -y && apt upgrade -y 
+  sleep 2 
+  echo 
+  sleep 2 
+  echo " instalando Python,python2 + o firewall " 
+  sleep 2
+  apt install python -y && apt install python2 -y && pip install wafw00f
+  echo 
+  sleep 2
+  wafw00f -h
